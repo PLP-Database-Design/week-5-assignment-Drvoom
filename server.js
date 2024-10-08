83cd0d66a338354a4d3f06e7f182dacce4f81356
Garba Ifeoluwa Joshua <142104585+Drvoom@users.noreply.github.com>
2024-10-08 22:37:25 +0100

    Update completed
    done

---
+++ server.js
@@ -0,0 +1,50 @@
+//question 1
+
+app.get('/patients', (req, res) => {
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients';
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(200).json(results);
+  });
+});
+
+//question 2
+
+app.get('/patients/:first_name', (req, res) => {
+  const { first_name } = req.params;
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients WHERE first_name = ?';
+  db.query(query, [first_name], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(200).json(results);
+  });
+});
+
+question 3
+
+app.get('/patients/:first_name', (req, res) => {
+  const { first_name } = req.params;
+  const query = 'SELECT patient_id, first_name, last_name, date_of_birth FROM patients WHERE first_name = ?';
+  db.query(query, [first_name], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(200).json(results);
+  });
+});
+
+question 4
+
+app.get('/providers/specialty/:specialty', (req, res) => {
+  const { specialty } = req.params;
+  const query = 'SELECT first_name, last_name, provider_specialty FROM providers WHERE provider_specialty = ?';
+  db.query(query, [specialty], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(200).json(results);
+  });
+});
